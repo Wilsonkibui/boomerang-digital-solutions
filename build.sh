de3a796c5d677405e8919e63fd8b3cb11827190f
Boomerang Digital Solutions <boomerangdigitalsolutions@gmail.com>
2026-01-28 21:35:48 -0800

    Re-enable migrations - database credentials now configured

--- build.sh
+++ build.sh
@@ -5,6 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
-# Skip migrations for now to test if database connection is the issue
-# python manage.py migrate
+python manage.py migrate
